bdeb236bef4f60a7d7f788c6e687cc04fe31b9a5
hasbi030 <113187476+hasbi030@users.noreply.github.com>
2022-09-23 19:05:43 +0700

    Update urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
-
+from django.urls import 
+    hasbi
 from . import views
-
+hasbi
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry"),
+    path("hasbi", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, hasbi="entry"),
     path("search", views.search, name="search"),
     path("add", views.add, name="add"),
+
     path("edit/<title>", views.edit, name="edit"),
     path("random_query", views.random_query, name="random_query")
 ]
